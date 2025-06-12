33ab834bec652f2fc879236fce28f1a576724d0e
kodrx <kodrx1@gmail.com>
2025-06-11 20:16:37 -0600

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,20 @@
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIjaOe4HcGNDk0xrqen8etBv0RyjyOJHw",
+  authDomain: "kodrx-105b9.firebaseapp.com",
+  databaseURL: "https://kodrx-105b9-default-rtdb.firebaseio.com",
+  projectId: "kodrx-105b9",
+  storageBucket: "kodrx-105b9.firebasestorage.app",
+  messagingSenderId: "239675098141",
+  appId: "1:239675098141:web:152ae3741b0ac79db7f2f4"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
