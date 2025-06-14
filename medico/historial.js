0e66e3293a47a5271768e36626a5ac4a56488fd9
kodrx <kodrx1@gmail.com>
2025-06-13 20:25:10 -0600

    Add files via upload

--- medico/historial.js
+++ medico/historial.js
@@ -2,7 +2,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getFirestore, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { firebaseConfig } from "/firebase-init.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIjaOe4HcGNDk0xrqen8etBv0RyjyOJHw",
+  authDomain: "kodrx-105b9.firebaseapp.com",
+  databaseURL: "https://kodrx-105b9-default-rtdb.firebaseio.com",
+  projectId: "kodrx-105b9",
+  storageBucket: "kodrx-105b9.appspot.com",
+  messagingSenderId: "239675098141",
+  appId: "1:239675098141:web:152ae3741b0ac79db7f2f4"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
