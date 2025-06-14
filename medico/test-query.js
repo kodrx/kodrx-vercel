d5519acd17d34cc396a1f77012b537932a6e5360
kodrx <kodrx1@gmail.com>
2025-06-13 20:08:00 -0600

    Add files via upload

---
+++ medico/test-query.js
@@ -0,0 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getFirestore, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIjaOe4HcGNDk0xrqen8etBv0RyjyOJHw",
+  authDomain: "kodrx-105b9.firebaseapp.com",
+  databaseURL: "https://kodrx-105b9-default-rtdb.firebaseio.com",
+  projectId: "kodrx-105b9",
+  storageBucket: "kodrx-105b9.appspot.com",
+  messagingSenderId: "239675098141",
+  appId: "1:239675098141:web:152ae3741b0ac79db7f2f4"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, async (user) => {
+  if (!user) {
+    console.log("No hay sesión iniciada");
+    return;
+  }
+
+  console.log("UID autenticado:", user.uid);
+
+  const q = query(collection(db, "recetas"), where("uid", "==", user.uid));
+  const snapshot = await getDocs(q);
+
+  console.log("Recetas encontradas:", snapshot.size);
+  snapshot.forEach(doc => {
+    console.log("Receta ID:", doc.id, doc.data());
+  });
+});
