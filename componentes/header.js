c83d36a415d7aaa06f0f6c89a6dd38d8120fe958
kodrx <kodrx1@gmail.com>
2025-06-18 20:19:13 -0600

    Update header.js

--- componentes/header.js
+++ componentes/header.js
@@ -2,7 +2,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const header = document.createElement("header");
   header.innerHTML = `
+      <a href="/index.html" class="logo-link">
     <img src="/img/logo-kodrx.png" class="logo" alt="KodRx Logo" />
+  </a>
     <h1>KodRx</h1>
   `;
   document.body.insertBefore(header, document.body.firstChild);
