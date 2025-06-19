e5037f15e619732bb958bc4b2c082756c98ccde9
kodrx <kodrx1@gmail.com>
2025-06-18 21:03:46 -0600

    Update header.js

--- componentes/header.js
+++ componentes/header.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const usaLogoClaro = !window.location.pathname.includes("/index.html");
-
   const header = document.createElement("header");
+
+  // Detecta si estás en index.html
+  const esIndex = location.pathname.endsWith("/index.html") || location.pathname === "/";
+
+  // Decide el logo según la página
+  const logoSrc = esIndex ? "/img/logo-kodrx-blanco.png" : "/img/logo-kodrx-color.png";
+
   header.innerHTML = `
     <a href="/index.html" class="logo-link">
-      <img src="/img/${usaLogoClaro ? 'logo-kodrx-blanco.png' : 'logo-kodrx-color.png'}" class="logo" alt="KodRx Logo" />
+      <img src="${logoSrc}" class="logo" alt="KodRx Logo" />
     </a>
   `;
+
   document.body.insertBefore(header, document.body.firstChild);
 });
