200ccca3f3144b62193d85db7a65d880882abbb0
kodrx <kodrx1@gmail.com>
2025-07-20 17:52:47 -0600

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,8 +1,15 @@
 
+// firebase-init.js universal para KodRx
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import {
+  getAuth,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
+// Configuración única del proyecto KodRx
 const firebaseConfig = {
   apiKey: "AIzaSyBIjaOe4HcGNDk0xrqen8etBv0RyjyOJHw",
   authDomain: "kodrx-105b9.firebaseapp.com",
@@ -13,9 +20,10 @@ const firebaseConfig = {
   appId: "1:239675098141:web:152ae3741b0ac79db7f2f4"
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-
-export { app, auth, db };
+// Exponer todo lo necesario desde un solo lugar
+export { app, auth, db, signOut, onAuthStateChanged };
