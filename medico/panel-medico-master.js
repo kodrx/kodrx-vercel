83fdba04e74316b1ddaa77e1f9491ebfba5049d6
kodrx <kodrx1@gmail.com>
2025-07-25 14:44:13 -0600

    Update panel-medico-master.js

--- medico/panel-medico-master.js
+++ medico/panel-medico-master.js
@@ -98,7 +98,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function agregarMedicamento() {
     const div = document.createElement("div");
     div.classList.add("medicamento");
-    div.innerHTML = \`
+    div.innerHTML = `
       <input type="text" class="nombre" placeholder="Nombre del medicamento">
       <input type="text" class="dosis" placeholder="Dosis">
       <input type="text" class="duracion" placeholder="Duración">
