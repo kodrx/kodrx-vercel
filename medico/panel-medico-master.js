0f56f6c24433ceae01b929329fa242bb275c206e
kodrx <kodrx1@gmail.com>
2025-07-25 23:40:04 -0600

    Update panel-medico-master.js

--- medico/panel-medico-master.js
+++ medico/panel-medico-master.js
@@ -2,6 +2,7 @@
 // 🚀 Script maestro activo para panel médico
 import { db, auth } from "/firebase-init.js";
 import { collection, addDoc, updateDoc, doc, Timestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getDoc, doc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("🧩 DOM cargado");
