edbd611423f031909fb6f17103c175d03061c385
kodrx <kodrx1@gmail.com>
2025-07-25 22:24:02 -0600

    Update verificador.js

--- farmacia/verificador.js
+++ farmacia/verificador.js
@@ -1,6 +1,6 @@
 import { db, auth } from '../firebase-init.js';
-import { doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 let datosFarmacia = null;
 let recetaGlobal = null;
