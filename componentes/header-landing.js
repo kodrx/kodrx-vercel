a990dfe195f9df7be3c5acf9cf9629c7888e911c
kodrx <kodrx1@gmail.com>
2025-07-30 14:44:07 -0600

    Update header-landing.js

--- componentes/header-landing.js
+++ componentes/header-landing.js
@@ -3,7 +3,7 @@
 const headerHTML = `
   <header class="header-landing">
     <div class="logo-landing">
-      <img src="/assets/logo.png" alt="KodRx logo" />
+      <img src="/img/logo-kodrx-color.png" alt="KodRx logo" />
       <span class="tagline">KodRx | Validación médica en blockchain</span>
     </div>
   </header>
