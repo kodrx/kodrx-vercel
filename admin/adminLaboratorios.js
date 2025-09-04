ab4bd71a10af4a24ed850a79271418f9bcd56732
kodrx <kodrx1@gmail.com>
2025-09-04 13:30:55 -0600

    Update adminLaboratorios.js

--- admin/adminLaboratorios.js
+++ admin/adminLaboratorios.js
@@ -1,6 +1,6 @@
 
 import { auth, db } from "../firebase-init.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 import {
   collection,
   addDoc,
