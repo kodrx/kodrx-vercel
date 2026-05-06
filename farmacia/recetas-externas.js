b262a830473c4fdc56e9c99e682a0c1ac508e94d
kodrx <kodrx1@gmail.com>
2026-05-05 22:04:45 -0600

    Update recetas-externas.js

--- farmacia/recetas-externas.js
+++ farmacia/recetas-externas.js
@@ -28,15 +28,7 @@ import {
   Dev:
   Aquí debes pegar el mismo firebaseConfig que ya usa KodRx.
 */
-const firebaseConfig = {
-  apiKey: "AIzaSyBIjaOe4HcGNDk0xrqen8etBv0RyjyOJHw",
-  authDomain: "kodrx-105b9.firebaseapp.com",
-  databaseURL: "https://kodrx-105b9-default-rtdb.firebaseio.com",
-  projectId: "kodrx-105b9",
-  storageBucket: "kodrx-105b9.firebasestorage.app",
-  messagingSenderId: "239675098141",
-  appId: "1:239675098141:web:152ae3741b0ac79db7f2f4"
-};
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
